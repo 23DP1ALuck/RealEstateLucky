430a557e8c61e686bc0fe990c84cb18eecc44545
t0mmyq <a231660al@gmail.com>
2025-02-23 15:08:31 +0200

    Add emailsend logic, all tests passed correctrly. Now check from PC and will add last section

--- src/js/main.js
+++ src/js/main.js
@@ -29,10 +29,55 @@ const message = document.getElementById('MessageText');
 
 const submitBtn = document.getElementById('submitBtn');
 
-function clearInput(event){
-    event.preventDefault();
+function clearInput(){
     email.value = '';
     fullName.value = '';
     subject.value = '';
     message.value = '';
+}
+function sendemail(){
+    parms = {
+        email: email.value,
+        fullName: fullName.value,
+        subject: subject.value,
+        messageText: message.value
+    }
+    if (parms.email === '' || parms.fullName === '' || parms.subject === '' || parms.messageText === ''){
+        console.log('Please fill in all fields')
+        showErrorNotification();
+    }else{
+        console.log('Email sent')
+        emailjs.send('service_kkh7r3e', 'template_kzcwdjc', parms)
+        showNotification();
+    }
+}
+function handleSubmit(event){
+    event.preventDefault();
+    sendemail();
+    clearInput();
+}
+
+submitBtn.addEventListener('click', handleSubmit)
+
+const emailSentNotification = document.getElementById('emailSentNotification');
+function showNotification(){
+    emailSentNotification.classList.add('show');
+    setTimeout(() => {
+        emailSentNotification.classList.add('hide');
+        setTimeout(() => {
+            emailSentNotification.classList.remove('show');
+            emailSentNotification.classList.remove('hide');
+        }, 1000);
+    }, 3000);
+}
+const errorNotification = document.getElementById('emailErrorNotification')
+function showErrorNotification(){
+    errorNotification.classList.add('show');
+    setTimeout(() =>{
+        errorNotification.classList.add('hide');
+        setTimeout(() =>{
+            errorNotification.classList.remove('show');
+            errorNotification.classList.remove('hide');
+        }, 1000);
+    }, 3000);
 }
